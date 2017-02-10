4577ec5daf8e1c3cd1ff4a347b727fbf4155c05e
maxdheilly <maxence.dheilly@devinci.fr>
2017-02-10 15:05:09 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,3 +8,45 @@ xmlhttp.onreadystatechange = function() {
 };
 xmlhttp.open("GET", "https://api.blockcypher.com/v1/btc/main", true);
 xmlhttp.send();
+
+function homePageLoading() {
+    url = 'apiUrl';
+    elementID = 'idDeVotreElementHtml';
+    fonctionRequeteApi(url, elementID);
+    
+    var isAddress = /^[0-9a-zA-Z]{34}$/.test(data);
+    
+    function output(inp) {
+    document.body.appendChild(document.createElement('pre')).innerHTML = inp;
+}
+
+function syntaxHighlight(json) {
+    json = json.replace(/&/g, '&').replace(//g, '>');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
+
+output(str);
+output(syntaxHighlight(str));
+    
+    Block -> Transactions
+Transaction -> Block
+Transaction -> Address
+Address -> Transaction
+    
+    data = `<a href="javascript:appelFonction('${data}')">"${data}"</a>`
+    
+}
